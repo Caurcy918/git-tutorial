f57aa29e12a2e2b4032b60c08eaada706aec19dc
Caurcy918 <ihouelecaurcy@gmail.com>
2024-07-09 21:37:16 +0200

    Docker notes 2

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int y = 5;
+    int8_t x = 8;
+    auto f = [=]()
+    {
+        return x + y;
+    };
+    std::cout << "Result: " << f() << '\n'; // Output: Result: 13
+    std::cout << "Result: " << f() << "Salut" << '\n'; //
+
+    return 0;
+}
