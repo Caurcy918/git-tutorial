6ba1a0c7e0992cb04f48cabda369ec3b68d96973
Caurcy918 <ihouelecaurcy@gmail.com>
2024-07-10 18:10:31 +0200

    Docker notes new version 2.3

--- ConsoleApplication1/main.js
+++ ConsoleApplication1/main.js
@@ -9,6 +9,14 @@ app.use(express.json());
 
 // Middleware to validate input data
 
+async function validate(req, res, next) {
+    // Validate user input here
+    const { username, password } = req.body;
+    if (!username ||!password) {
+        return res.status(400).json({ error: 'Username and password are required' });
+    }
+    next();
+}
 
 // Middleware to authenticate requests
 
